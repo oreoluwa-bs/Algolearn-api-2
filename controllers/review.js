fc81c98ae434b7f303783dc2cf8f8617eb4ee28e
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2020-04-04 06:20:42 +0100

    Implemented review controller

---
+++ controllers/review.js
@@ -0,0 +1,12 @@
+const Review = require('../models/review');
+const factory = require('./handlerFactory');
+
+exports.getAllReviews = factory.getAll(Review);
+
+exports.getReview = factory.getOne(Review);
+
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
+
+exports.deleteReview = factory.deleteOne(Review);
